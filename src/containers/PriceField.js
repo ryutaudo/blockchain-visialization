9a7fdf4c4dccc9fcd0846b457b890b4fa4f3c034
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-02-13 16:06:41 +0900

    add PriceField container

--- src/containers/PriceField.js
+++ src/containers/PriceField.js
@@ -1,4 +1,13 @@
 import { connect } from 'react-redux';
 import PriceField from '../components/PriceField';
+import { getPrice } from '../actions/index';
 
-export default connect()(PriceField);
+const mapStateToProps = state => ({
+  price: state.price,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getPrice: () => dispatch(getPrice()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PriceField);
